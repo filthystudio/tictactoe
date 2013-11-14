4e12ea14023ad3f417532c6b873d73cbb35c304d
TonoFilth <tonofilth@gmail.com>
2013-11-14 14:17:48 +0100

    Project tictactoe created for Desktop(Windows, Linux, Mac) and Android

---
+++ tictactoe-android/gen/com/filthystudio/games/tictactoe/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.filthystudio.games.tictactoe;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ tictactoe-android/src/com/filthystudio/games/tictactoe/MainActivity.java
@@ -0,0 +1,19 @@
+package com.filthystudio.games.tictactoe;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+public class MainActivity extends AndroidApplication {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
+        cfg.useGL20 = false;
+        cfg.useWakelock = true;
+        
+        initialize(new FsGame(), cfg);
+    }
+}
